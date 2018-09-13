75445ed9be4cfb3c3a18cb9ea261daaec295486a
Alan Agius <alan.agius4@gmail.com>
2018-09-13 18:58:30 +0200

    fix everything

--- runner.sh
+++ runner.sh
@@ -1,36 +1,39 @@
 #!/bin/bash
 
+setup_and_benchmark() {
+    rm -rf node_modules/@angular node_modules/typescript node_modules/@angular-devkit @ngtools/webpack
+    npm i --loglevel error --no-audit --no-package-lock
+    node_modules/.bin/benchmark -- npm run build -- --prod
+}
+
+npm i --no-package-lock
+
 echo -e "\n------------------------------------------------------"
 echo "Benchmarking using TypeScript 2.9.2 and Angular 6"
 echo "------------------------------------------------------"
 cp package-ng-6-ts-2-9.json package.json
-npm i --loglevel error --no-audit
-node_modules/.bin/benchmark -- ng build --prod
+setup_and_benchmark
 
 echo -e "\n------------------------------------------------------"
 echo "Benchmarking using TypeScript 3.0.1 and Angular 6"
 echo "------------------------------------------------------"
 cp package-ng-7-ts-3-0.json package.json
-npm i --loglevel error --no-audit
-node_modules/.bin/benchmark -- ng build --prod
+setup_and_benchmark
 
 echo -e "\n------------------------------------------------------"
 echo "Benchmarking using TypeScript 3.0.1 and Angular 7"
 echo "------------------------------------------------------"
 cp package-ng-7-ts-3-0.json package.json
-npm i --loglevel error --no-audit
-node_modules/.bin/benchmark -- ng build --prod
+setup_and_benchmark
 
 echo -e "\n------------------------------------------------------"
 echo "Benchmarking using TypeScript 2.9.2 and Angular 7"
 echo "------------------------------------------------------"
 cp package-ng-7-ts-2-9.json package.json
-npm i --loglevel error --no-audit
-node_modules/.bin/benchmark -- ng build --prod
+setup_and_benchmark
 
 echo -e "\n------------------------------------------------------"
 echo "Benchmarking using TypeScript 3 and Angular 7, Angular CLI Beta"
 echo "------------------------------------------------------"
 cp package-ng-7-ts-3-0-cli-beta.json package.json
-npm i --loglevel error --no-audit
-node_modules/.bin/benchmark -- ng build --prod
+setup_and_benchmark
