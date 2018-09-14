1603945231cdb700be0e8c91798dd486e5b72529
Alan Agius <alan.agius4@gmail.com>
2018-09-14 18:02:50 +0200

    remove all node_modules

--- runner.sh
+++ runner.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 setup_and_benchmark() {
-    rm -rf node_modules/@angular node_modules/typescript node_modules/@angular-devkit node_modules/@ngtools/webpack node_modules/rxjs
+    # rm -rf node_modules/@angular node_modules/typescript node_modules/@angular-devkit node_modules/@ngtools/webpack node_modules/rxjs
+    rm -rf node_modules
     npm i --loglevel error --no-audit --no-package-lock
     npm list typescript
     node_modules/.bin/benchmark -- npm run build -- --prod
