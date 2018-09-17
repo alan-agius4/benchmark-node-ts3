176b61521b1ab3ec2174ca22faaca117400f4e24
Alan Agius <alan.agius4@gmail.com>
2018-09-17 10:42:10 +0200

    no bo

--- runner.sh
+++ runner.sh
@@ -5,7 +5,7 @@ setup_and_benchmark() {
     rm -rf node_modules
     npm i --loglevel error --no-audit --no-package-lock
     npm list typescript
-    node_modules/.bin/benchmark -- npm run build -- --prod
+    node_modules/.bin/benchmark -- npm run build -- --prod --build-optimizer=false
 }
 
 echo -e "\n-----------------------------------------------------------"
@@ -14,23 +14,23 @@ echo "-----------------------------------------------------------"
 cp package-ng-6-ts-2-9.json package.json
 setup_and_benchmark
 
-# echo -e "\n-----------------------------------------------------------"
-# echo "Benchmarking using TypeScript 2.9, Angular 6 & Angular CLI 7"
-# echo "-----------------------------------------------------------"
-# cp package-ng-6-ts-2-9-cli-7.json package.json
-# setup_and_benchmark
+echo -e "\n-----------------------------------------------------------"
+echo "Benchmarking using TypeScript 2.9, Angular 6 & Angular CLI 7"
+echo "-----------------------------------------------------------"
+cp package-ng-6-ts-2-9-cli-7.json package.json
+setup_and_benchmark
 
-# echo -e "\n-----------------------------------------------------------"
-# echo "Benchmarking using TypeScript 3, Angular 6 & Angular CLI 6"
-# echo "-----------------------------------------------------------"
-# cp package-ng-6-ts-3-0.json package.json
-# setup_and_benchmark
+echo -e "\n-----------------------------------------------------------"
+echo "Benchmarking using TypeScript 3, Angular 6 & Angular CLI 6"
+echo "-----------------------------------------------------------"
+cp package-ng-6-ts-3-0.json package.json
+setup_and_benchmark
 
-# echo -e "\n-----------------------------------------------------------"
-# echo "Benchmarking using TypeScript 3, Angular 7 & Angular CLI 6"
-# echo "------------------------------------------------------"
-# cp package-ng-7-ts-3-0.json package.json
-# setup_and_benchmark
+echo -e "\n-----------------------------------------------------------"
+echo "Benchmarking using TypeScript 3, Angular 7 & Angular CLI 6"
+echo "------------------------------------------------------"
+cp package-ng-7-ts-3-0.json package.json
+setup_and_benchmark
 
 echo -e "\n-----------------------------------------------------------"
 echo "Benchmarking using TypeScript 2.9, Angular 7 & Angular CLI 6"
@@ -38,8 +38,8 @@ echo "-----------------------------------------------------------"
 cp package-ng-7-ts-2-9.json package.json
 setup_and_benchmark
 
-# echo -e "\n-----------------------------------------------------------"
-# echo "Benchmarking using TypeScript 3, Angular 7 & Angular CLI 7"
-# echo "-----------------------------------------------------------"
-# cp package-ng-7-ts-3-0-cli-7.json package.json
-# setup_and_benchmark
+echo -e "\n-----------------------------------------------------------"
+echo "Benchmarking using TypeScript 3, Angular 7 & Angular CLI 7"
+echo "-----------------------------------------------------------"
+cp package-ng-7-ts-3-0-cli-7.json package.json
+setup_and_benchmark
