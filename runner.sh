1423ec08353e955d2733208fcf34b979cc4598cd
Alan Agius <alan.agius4@gmail.com>
2018-09-17 20:33:31 +0200

    change order

--- runner.sh
+++ runner.sh
@@ -8,6 +8,13 @@ setup_and_benchmark() {
     node_modules/.bin/benchmark -- npm run build-clean
 }
 
+
+echo -e "\n-----------------------------------------------------------"
+echo "Benchmarking using TypeScript 3, Angular 7 & Angular CLI 7"
+echo "-----------------------------------------------------------"
+cp package-ng-7-ts-3-0-cli-7.json package.json
+setup_and_benchmark
+
 echo -e "\n-----------------------------------------------------------"
 echo "Benchmarking using TypeScript 2.9, Angular 7 & Angular CLI 7"
 echo "-----------------------------------------------------------"
@@ -48,10 +55,4 @@ echo -e "\n-----------------------------------------------------------"
 echo "Benchmarking using TypeScript 2.9, Angular 7 & Angular CLI 6"
 echo "-----------------------------------------------------------"
 cp package-ng-7-ts-2-9.json package.json
-setup_and_benchmark
-
-echo -e "\n-----------------------------------------------------------"
-echo "Benchmarking using TypeScript 3, Angular 7 & Angular CLI 7"
-echo "-----------------------------------------------------------"
-cp package-ng-7-ts-3-0-cli-7.json package.json
 setup_and_benchmark
