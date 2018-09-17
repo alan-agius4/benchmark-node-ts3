91f4d38f215302e43fcb9aa1aeb604b86babe102
Alan Agius <alan.agius4@gmail.com>
2018-09-17 16:10:10 +0200

    add missing permutations

--- runner.sh
+++ runner.sh
@@ -1,12 +1,24 @@
 #!/bin/bash
 
 setup_and_benchmark() {
-    # rm -rf node_modules/@angular node_modules/typescript node_modules/@angular-devkit node_modules/@ngtools/webpack node_modules/rxjs
     rm -rf node_modules
     npm i --loglevel error --no-audit --no-package-lock
     npm list typescript
-    node_modules/.bin/benchmark -- npm run build -- --prod --build-optimizer=false
+    npm run build -- --prod
+    node_modules/.bin/benchmark -- rm -rf node_modules/.cache && npm run build -- --prod
 }
+
+echo -e "\n-----------------------------------------------------------"
+echo "Benchmarking using TypeScript 2.9, Angular 7 & Angular CLI 7"
+echo "-----------------------------------------------------------"
+cp package-ng-7-ts-2-9-cli-7.json package.json
+setup_and_benchmark
+
+echo -e "\n-----------------------------------------------------------"
+echo "Benchmarking using TypeScript 2.9, Angular 7 & Angular CLI 7"
+echo "-----------------------------------------------------------"
+cp package-ng-7-ts-2-9-cli-7.json package.json
+setup_and_benchmark
 
 echo -e "\n-----------------------------------------------------------"
 echo "Benchmarking using TypeScript 2.9, Angular 6 & Angular CLI 6"
@@ -30,6 +42,12 @@ echo -e "\n-----------------------------------------------------------"
 echo "Benchmarking using TypeScript 3, Angular 7 & Angular CLI 6"
 echo "------------------------------------------------------"
 cp package-ng-7-ts-3-0.json package.json
+setup_and_benchmark
+
+echo -e "\n-----------------------------------------------------------"
+echo "Benchmarking using TypeScript 3, Angular 6 & Angular CLI 7"
+echo "------------------------------------------------------"
+cp package-ng-6-ts-3-cli-7.json package.json
 setup_and_benchmark
 
 echo -e "\n-----------------------------------------------------------"
