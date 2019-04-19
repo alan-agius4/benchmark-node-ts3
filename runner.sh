befccf9c768307fb5511b81545ca7421a4923130
Alan Agius <alan.agius4@gmail.com>
2019-04-19 07:04:16 +0000

    add browserslist

--- runner.sh
+++ runner.sh
@@ -8,21 +8,31 @@ setup_and_benchmark() {
     node_modules/.bin/benchmark -- npm run build-clean
 }
 
+
+echo -e "\n-----------------------------------------------------------"
+echo "Benchmarking using No DL"
+echo "-----------------------------------------------------------"
+setup_and_benchmark
+
+
 echo -e "\n-----------------------------------------------------------"
 echo "Benchmarking using DL parrallel"
 echo "-----------------------------------------------------------"
+cp overrides/browserslist src/browserslist
 setup_and_benchmark
 
 
 echo -e "\n-----------------------------------------------------------"
 echo "Benchmarking using DL Sequential"
 echo "-----------------------------------------------------------"
+cp overrides/browserslist src/browserslist
 cp overrides/sequntial-index.js node_modules/@angular-devkit/build-angular/src/browser/index.js
 setup_and_benchmark
 
 echo -e "\n-----------------------------------------------------------"
-echo "Benchmarking using MultiCompiler"
+echo "Benchmarking using DL MultiCompiler"
 echo "-----------------------------------------------------------"
+cp overrides/browserslist src/browserslist
 cp overrides/multicompiler/index.js node_modules/@angular-devkit/build-angular/src/browser/index.js
 cp overrides/multicompiler/webpack.js node_modules/@angular-devkit/build-webpack/src/webpack/index.js
 setup_and_benchmark
